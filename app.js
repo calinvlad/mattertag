ba652599da694cba8e0580bd228fe1b8e36d6208
calinvlad <calinvlad.t@gmail.com>
2018-08-29 03:11:17 +0300

    test source

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser");
 
-    app.use(express.static(__dirname + "/public"));
+    app.use(express.static(__dirname + "/clients"));
+    app.set("view engine", "ejs");
     app.use(bodyParser.urlencoded({extended: true}));
 
-    // app.get('/', function(req, res) {
-    //     res.render('home')
-    // })
+    app.get('/', function(req, res) {
+        res.render('index.ejs')
+    })
+
+
+    app.listen(5000, function(){
+        console.log("Mattertag server on");
+     });
